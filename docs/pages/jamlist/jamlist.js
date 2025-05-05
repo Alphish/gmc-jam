70413f47bc218e3e490f710d451588409d0493c3
Alice Jankowska <alphish.creature@gmail.com>
2025-05-05 23:44:11 +0200

    ISS-7: Moved and reorganised styles and scripts

--- docs/pages/jamlist/jamlist.js
+++ docs/pages/jamlist/jamlist.js
@@ -5,7 +5,7 @@ fetch("/jams/jamlist.json")
         for (let jam of jamlist) {
             let row = tableEntries.insertRow();
             row.innerHTML = 
-                `<td class="jam-title">${jam.title}</td>` +
+                `<td class="jam-title"><a href="/jamview.html?id=${jam.id}">${jam.title}</a></td>` +
                 `<td class="jam-theme">${jam.theme}</td>` +
                 `<td class="jam-date">${jam.date}</td>` +
                 `<td class="jam-hosts">${jam.hosts}</td>`;
